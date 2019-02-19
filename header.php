2237e35de363f84be06ce994bab92136e60f21ba
unknown <jerson.huayta@tecsup.edu.pe>
2019-02-19 18:49:26 -0500

    Commit

---
+++ header.php
@@ -0,0 +1,104 @@
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Exertion an Industrial Category Bootstrap Responsive Website Template | Home :: w3layouts</title>
+	<!-- Meta Tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content="Exertion Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //Meta Tags -->
+	<!-- Style-sheets -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<!--// Style-sheets -->
+		<link rel="stylesheet" type="text/css" href="css/easy-responsive-tabs.css " />
+	<!--web-fonts-->
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Raleway:400,500,600,700,800" rel="stylesheet">
+	<!--//web-fonts-->
+</head>
+
+<body>
+	<!-- banner -->
+	<section class="banner">
+		<!-- header -->
+		<header>
+		<nav class="navDark">
+			<div class="container">
+				dfghj
+			</div>
+		</nav>
+		<nav class="navbar navbar-expand-lg navbar-light">
+		<div class="container">
+			<a class="navbar-brand" href="index.html"><img src="final.png" width="250"></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+			    aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="index.html">Inicio
+							<span class="sr-only">(current)</span>
+						</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+						    aria-expanded="false">
+							Nosotros
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item text-center" href="projects.html">Projects</a>
+							<a class="dropdown-item text-center" href="typography.html">Typography</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+						    aria-expanded="false">
+							Servicios
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item text-center" href="projects.html">Projects</a>
+							<a class="dropdown-item text-center" href="typography.html">Typography</a>
+						</div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="index.html">Galeria
+						
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="index.html">Clientes
+						
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="contact.html">Contacto</a>
+					</li>
+				</ul>
+				
+			</div>
+			</div>
+		</nav>
+		
+		</header>
